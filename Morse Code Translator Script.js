f36336fbe89da3ea7d2c3a0554c98493179dc7d1
Seasupermike <mkiwa37@gmail.com>
2025-06-11 17:34:42 -0400

    Update Morse Code Translator Script.js

--- Morse Code Translator Script.js
+++ Morse Code Translator Script.js
@@ -168,6 +168,42 @@ function CopyMorseCode() {
     navigator.clipboard.writeText(CopyableText)
 }
 
+async function TemporarilyChangeClass(Id, Time, Class) {
+    // Time is in milliseconds
+    $(Id).addClass(Class);
+    await Delay(Time);
+    $(Id).removeClass(Class);
+}
+
+async function Delay(Time) {
+    return new Promise(Resolve => setTimeout(Resolve, Time))
+}
+
+function CheckForDupilactes(Arr, Target) {
+    for (let i = 0; i < Arr.length; i++) {
+        if (Arr[i] == Target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function CountCharacters(Text, Target) {
+    let Characters = Text.split("");
+    if (Target == undefined) {
+        return Characters.length;
+    }
+
+    let Count = 0;
+    for (let i = 0; i < Characters.length; i++) {
+        if (Text[i] == Target) {
+            Count++;
+        }
+    }
+
+    return Count;
+}
+
 const Codes = [
     { Uppercase: `A`, Lowercase: `a`, Morse: `._` },
     { Uppercase: `B`, Lowercase: `b`, Morse: `_...` },
